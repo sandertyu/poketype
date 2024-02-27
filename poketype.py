f7f2343c9b7a81df093a26a9be5d68a88c39330e
sandertyu <noasanders@ucdavis.edu>
2024-02-26 16:47:14 -0800

    tierlist method, attempt pretty matchup method

--- poketype.py
+++ poketype.py
@@ -31,6 +31,7 @@ class TypeChart():
     def __init__(self):
         self.table = TABLE
         self.types = TYPES
+        self.map = {self.types[i]: i for i in range(NUM)}
 
     def get_chart(self):
         return self.table
@@ -58,19 +59,34 @@ class TypeChart():
         return (sum0,sum1)
 
     def score(self,ptype):
-        matchup = self.get_matchup(ptype)
-        tot = self.sum_matchup(matchup)
+        eff = self.get_matchup(ptype)
+        tot = self.sum_matchup(eff)
         return tot[0]+tot[1]
+
+    def tierlist(self):
+        scores = {t: typechart.score(t) for t in typechart.get_types()}
+        scorelist = sorted(scores.values(),reverse=True)
+        typelist = sorted(scores.values(),reverse=True)
+        for key,val in scores.items():
+            index = typelist.index(val)
+            typelist[index] = key
+        tierlist = {typelist[i]: scorelist[i] for i in range(NUM)}
+        return tierlist
+
+    '''
+    def matchup(self,ptype):
+        eff = self.get_matchup(ptype)
+        a,d = eff[0],eff[1]
+        for x in a:
+            if x != 0:
+                i = a.index(x)
+                attack[i] = self.get_ptype(x)
+        for y in d:
+            if y != 0:
+                i = d.index(y)
+                defense[i] = self.get_ptype(y)
+        return attack,defense
+        '''
 
 typechart = TypeChart()
 chart = typechart.get_chart()
-
-scores = {t: typechart.score(t) for t in typechart.get_types()}
-scorelist = sorted(scores.values(),reverse=True)
-typelist = sorted(scores.values(),reverse=True)
-
-for k,v in scores.items():
-    i = typelist.index(v)
-    typelist[i] = k
-
-tierlist = {typelist[i]: scorelist[i] for i in range(NUM)}
